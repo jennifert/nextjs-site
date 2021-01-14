e8eccc3c1090efbd6c6f3affeb300a55fa39432b
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2021-01-14 00:01:10 -0500

    Initial Files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    // fontSize: {
+    //   'xs': '.75rem',
+    //   'sm': '.875rem',
+    //   'base': '1rem',
+    //   'lg': '1.125rem',
+    //   'xl': '1.25rem',
+    //   '2xl': '1.5rem',
+    //   '3xl': '1.875rem',
+    //   '4xl': '2.25rem',
+    //   '5xl': '3rem',
+    //   '6xl': '4rem',
+    //   '7xl': '5rem',
+    // },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
