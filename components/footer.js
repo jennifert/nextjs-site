4db561d8bbce98db1e23c2bc2c5b832627892045
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2021-02-10 14:14:30 -0500

    Image Upates
    Changed to svg from font awesome. Also moved images from public folder to cloudinary.

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,30 @@
-
 import Image from 'next/image'
-export default function Footer() {
+
+const socialLoader = ({ src, width, quality }) => {
+    return `https://res.cloudinary.com/dkeghqshh/image/upload/v1612975704/jenntesolin.com/social${src}?w=${width}&q=${quality || 75}`
+}
+
+export default function Footer(props) {
     return (
         <footer>
                 <nav aria-label="Social Media">
                 <ul className="flex justify-center my-5">
                         <li>
                         <a className="p-2 m-2" href="https://github.com/jennifert?tab=repositories" target="_blank" rel="nofollow noreferrer" aria-label="Github Repositories">
-                                <Image
-                                    src="/imgs/social-041-round_github.png"
+                            <Image
+                                loader={socialLoader}
+                                src="/github-square_nmpyub.svg"
                                     alt="Github"
                                     width={30}
-                                    height={30}
+                                height={30}
                                 />
                             </a>
                         </li>
                         <li>
                         <a className="p-2 m-2" href="https://twitter.com/jenntes" target="_blank" rel="nofollow noreferrer" aria-label="Twitter Feed">
-                                <Image
-                                    src="/imgs/social-005-round_twitter.png"
+                            <Image
+                                loader={socialLoader}
+                                src="/twitter-square_zgyods.svg"
                                     alt="Twitter"
                                     width={30}
                                     height={30}
@@ -27,8 +33,9 @@ export default function Footer() {
                         </li>
                         <li>
                         <a className="p-2 m-2" href="/feed.xml" rel="nofollow noreferrer" aria-label="Site Feed">
-                                <Image
-                                src="/imgs/social-020-round_rss.png"
+                            <Image
+                                loader={socialLoader}
+                                src="/rss-square_hsohh9.svg"
                                     alt="RSS"
                                     width={30}
                                     height={30}
@@ -37,8 +44,9 @@ export default function Footer() {
                         </li>
                         <li>
                         <a className="p-2 m-2" href="http://ca.linkedin.com/pub/jennifer-tesolin/2b/5bb/3a6" target="_blank" rel="nofollow noreferrer" aria-label="LinkedIn Profile">
-                                <Image
-                                src="/imgs/social-011-round_linkedin.png"
+                            <Image
+                                loader={socialLoader}
+                                src="/linkedin_t8xmbz.svg"
                                     alt="LinkedIn"
                                     width={30}
                                     height={30}
@@ -50,6 +58,7 @@ export default function Footer() {
                 <p>&copy; {(new Date().getFullYear())} Jennifer Tesolin.</p>
                 {/* <p>Header Image By: <a href="https://unsplash.com/@scottwebb" target="_blank" rel="nofollow">Scott
                 Webb</a></p> */}
+            <p>Icons by Font Awesome Pro 5</p>
             </footer>
     )
 }
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    images: {
+        loader: 'cloudinary',
+        path: 'https://res.cloudinary.com/dkeghqshh/image/upload/v1612975699/',
+    },
+}
