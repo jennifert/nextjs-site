41833d2df869e8a72009452bbaa5aa1a79fbda8b
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2021-08-12 13:15:13 -0400

    Updated NPM, minor style update and code fix.

--- pages/blog/2013-11-05-bootstrap-3-and-google-maps.js
+++ pages/blog/2013-11-05-bootstrap-3-and-google-maps.js
@@ -22,13 +22,13 @@ export default function BootstrapThreeGmap() {
             </Head>
             <section aria-labelledby="main-content">
                 <h1 id="main-content">{POST_TITLE}</h1>
-                <span className="alert-warning">
-                    <p>Notes:</p>
+                <div className="alert-warning">
+                    <p><b>Notes:</b></p>
                     <ol className="list-decimal list-inside m-2">
                         <li>This is an update to the older post using <a className="underline focus:ring-2" href="https://web.archive.org/web/20140921030553/http://jenntesolin.com/blog/2013/04/11/bootstrap-and-google-maps/" rel="nofollow noreferrer">Bootstrap 2 with Google Maps</a>.</li>
-                        <li>This post is outdated, and is in the queue for an overhaul. <a className="underline focus:ring-2" href="https://github.com/jennifert/JavaScript-Demos/tree/master/GoogleMaps/" rel="nofollow noreferrer">Check out github repo for the latest version.</a></li>
+                        <li>This post is outdated, and is in the queue for an overhaul. <a className="underline focus:ring-2" href="https://github.com/jennifert/JavaScript-Demos/tree/master/GoogleMaps/" rel="nofollow noreferrer">Check out github repo for the latest code.</a></li>
                     </ol>
-                </span>
+                </div>
                 
                 <p>There are changes in Bootstrap 3 that stops Google maps from displaying correctly in modals, or tabs.  This post described how to update your code from Bootstrap 2.</p>
                 
