2985688c1f5fd37f3fbe5bc170794d42fcfa5826
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2025-07-13 01:34:06 -0400

    Fixing meta tag component

--- components/meta.js
+++ components/meta.js
@@ -1,9 +1,9 @@
-// import Head from 'next/head'
+import Head from 'next/head';
 // import { SITE_DESCRIPTION, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
   return (
-    <>
+    <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link type="text/plain" rel="author" href="/humans.txt" />
@@ -11,6 +11,6 @@ export default function Meta() {
       <meta name="theme-color" content="#317EFB" />
       <meta name="google-site-verification" content="Q8UByE2wXyZ401kKeeWvvOkSSx3fW_vaGE-UO0Q_uDA" />
       {/* You can add OG tags here later if needed */}
-    </>
-  )
+    </Head>
+  );
 }
