0696012074411ad04b0755451aeefabbe3d529e7
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2026-04-17 18:29:56 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
