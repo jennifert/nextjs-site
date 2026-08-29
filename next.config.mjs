573c32160bc8a0a1c81522cc0bede7b7efcbbdff
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2026-08-28 21:33:55 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 import createMDX from '@next/mdx'
-/** @type {import('next').NextConfig} */
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
